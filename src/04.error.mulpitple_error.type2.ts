b67bb77bb6d0cb4ba8861c5d4193a09d4a9a6fa5
GUKBEOM LEE <gukbeomlee@GUKBEOMui-Macmini.local>
2023-04-27 17:28:11 +0900

    _

---
+++ src/04.error.mulpitple_error.type2.ts
@@ -0,0 +1,40 @@
+import * as Effect from '@effect/io/Effect'
+import * as Data from '@effect/data/Data'
+import * as Random from '@effect/io/Random'
+import {pipe} from '@effect/data/Function'
+import { tryCatch } from '@effect/io/Effect'
+
+interface FooError extends Data.Case {readonly _tag: "FooError"}   
+interface BarError extends Data.Case {readonly _tag: "BarError"}
+
+const FooError = Data.tagged<FooError>("FooError")
+const BarError = Data.tagged<BarError>("BarError")
+   
+const flakyBar = pipe(  
+    Random.next(),
+    Effect.flatMap((n) =>
+      n > 0.5
+        ? Effect.succeed("yay!")
+        : Effect.fail(BarError())
+    )
+)
+
+const program = pipe(
+                      pipe(
+                        Random.next(),
+                        Effect.flatMap((n) =>
+                          n >  0.5
+                            ? Effect.succeed("yay!")
+                            : Effect.fail(FooError())
+                        )
+                      ), 
+                      Effect.zipRight(flakyBar),                      
+                      Effect.catchTags({
+                        "FooError" : (error) => Effect.succeed(`recovery from ${error._tag}`),
+                        "BarError" : (error) => Effect.succeed(`recovery from ${error._tag}`),
+                      })
+                    )
+
+console.log(
+      Effect.runSync(program)
+);
