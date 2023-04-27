9b37193f437db5cc1e27ac785d78b89c0bf2cac4
GUKBEOM LEE <gukbeomlee@GUKBEOMui-Macmini.local>
2023-04-27 13:44:46 +0900

    _

---
+++ src/04.error.mulpitple_error.ts
@@ -0,0 +1,53 @@
+import * as Effect from '@effect/io/Effect'
+import * as Data from '@effect/data/Data'
+import * as Random from '@effect/io/Random'
+import {pipe} from '@effect/data/Function'
+import { tryCatch } from '@effect/io/Effect'
+
+interface FooError extends Data.Case {readonly _tag: "FooError"}   
+interface BarError extends Data.Case {readonly _tag: "BarError"}
+
+const FooError = Data.tagged<FooError>("FooError")
+const BarError = Data.tagged<BarError>("BarError")
+   
+const flakyFoo = pipe(
+    Random.next(),
+    Effect.flatMap((n) =>
+      n > 1
+        ? Effect.succeed("yay!")
+        : Effect.fail(FooError())
+    )
+)
+   
+const flakyBar = pipe(  
+    Random.next(),
+    Effect.flatMap((n) =>
+      n > 0
+        ? Effect.succeed("yay!")
+        : Effect.fail(BarError())
+    )
+)
+
+const program = pipe(
+                      pipe(
+                        Random.next(),
+                        Effect.flatMap((n) =>
+                          n > 1
+                            ? Effect.succeed("yay!")
+                            : Effect.fail(FooError())
+                        )
+                      ), 
+                      Effect.zipRight(flakyBar),
+                      // Effect.zipLeft(
+                      //   Effect.succeed(`effect.zipleft`)
+                      // ),
+                      Effect.catchTag("FooError", (fooError) => 
+                        Effect.succeed(`Recovering from ${fooError._tag}`)
+                      )
+                    )
+
+console.log(
+      Effect.runSync(program)
+);
+  
+  
